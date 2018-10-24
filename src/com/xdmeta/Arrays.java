bd191a6e1d0307308452be289b881af2cfdcdccf
aakash <akash.mehta@solutelabs.com>
2018-10-24 21:31:55 +0530

    integrate quick sort

--- src/com/xdmeta/Arrays.java
+++ src/com/xdmeta/Arrays.java
@@ -9,18 +9,59 @@ public class Arrays {
 
     private static final int ARRAY = 0;
     private static final int LINK_LIST = 1;
-
+    private static int[] ar;
     public static void main(String a[]) {
-        findMismatch(new int[]{10,3,5,7,2,8,1}, 4, ARRAY);
+//        findMismatch(new int[]{10, 3, 5, 7, 2, 8, 1}, 4, ARRAY);
+        ar = new int[]{6,3,7,2,1,8};
+        sort(ar);
+        for (int anAr : ar) {
+            System.out.println("ar[k] = " + anAr);
+        }
+
+    }
+
+
+    private static void sort(int[] ar) {
+        quickSort(0, ar.length - 1);
+    }
+
+    private static void quickSort(int low, int high) {
+        int pivot = ar[low + (high - low)/2];
+        int i = low, j = high;
+        while (i <= j) {
+            while (ar[i] < pivot) {
+                i++;
+            }
+            while (ar[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(i, j);
+                i++;
+                j--;
+            }
+        }
+        if (low < j) {
+            quickSort(low, j);
+        }
+        if (high > i) {
+            quickSort(  i, high);
+        }
+    }
+
+    private static void swap(int i, int j) {
+        int temp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = temp;
     }
 
     /**
      * Find a missing number in given integer array of 1 to 100?
-
-     Read more: http://www.java67.com/2018/06/data-structure-and-algorithm-interview-questions-programmers.html#ixzz5T86IXbtD
-
-     * @param array input array.
-     * @param i missing item count.
+     * <p>
+     * Read more: http://www.java67.com/2018/06/data-structure-and-algorithm-interview-questions-programmers.html#ixzz5T86IXbtD
+     *
+     * @param array    input array.
+     * @param i        missing item count.
      * @param strategy (optional) Used strategy to achieve task.
      */
     private static void findMismatch(int[] array, int i, int strategy) {
@@ -40,7 +81,7 @@ public class Arrays {
                 }
 
             }
-            case ARRAY : {
+            case ARRAY: {
                 int[] opArr = new int[i];
                 int m = 0;
                 for (int j = 0; j < array.length + i; j++) {
