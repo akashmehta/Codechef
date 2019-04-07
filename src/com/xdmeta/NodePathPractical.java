33305270f14cae91ea998845e5500aba51905ef4
akashMehta-STL <akash.mehta@solutelabs.com>
2019-04-07 20:46:55 +0530

    Resolve issue for test project.

--- src/com/xdmeta/NodePathPractical.java
+++ src/com/xdmeta/NodePathPractical.java
@@ -6,24 +6,20 @@ import java.util.*;
 
 class NodePathPractical {
     public static void main(String[] args) {
-        int[] output = solution(new int[] {2, 1, 1});
+        int[] output = solution(new int[] {9,1,4,9,0,4,8,9,0,1});
         System.out.println(Arrays.toString(output));
     }
 
     static HashMap<Integer, ArrayList<Integer>> map = new HashMap();
+    static int rootNode = -1;
     static int[] pathsAr = new int[0];
     public static int[] solution(int[] T) {
         // write your code in Java SE 8
-        int rootNode = -1;
-        int[] pathsAr = new int[T.length];
+        pathsAr = new int[T.length];
         Arrays.fill(pathsAr, 0);
 
         for (int i = 0; i < T.length ; i ++) {
-            if ( i == T[i]) {
-                rootNode = i;
-            }
-        }
-        for (int i = 0; i < T.length; i++) {
+            if ( i == T[i]) { rootNode = i; }
             if (map.get(T[i]) == null) {
                 ArrayList<Integer> valueAr = new ArrayList();
                 valueAr.add(i);
@@ -45,25 +41,29 @@ class NodePathPractical {
             }
             if (rootConnectedNodes.contains(T[i])) {
                 pathsAr[position] = pathsAr[position] + 1;
+                foundPaths.add(T[i]);
                 position ++;
             } else {
-                findPath(rootConnectedNodes, T[i], position, pathsAr[position]++);
+                findPath(rootConnectedNodes, T[i], position, ++pathsAr[position]);
+                foundPaths.add(T[i]);
                 position++;
             }
         }
         return pathsAr;
     }
 
-    private static void findPath(ArrayList<Integer> rootConnectedNodes, int dest, int stepIndex, int steps) {
+    private static boolean findPath(ArrayList<Integer> rootConnectedNodes, int dest, int stepIndex, int steps) {
         if (rootConnectedNodes.contains(dest)) {
-            pathsAr[stepIndex] = pathsAr[stepIndex] + steps;
+            pathsAr[stepIndex] = steps;
+            return true;
         } else {
             for (Integer node : rootConnectedNodes) {
-                if (map.containsKey(node)) {
+                if (map.containsKey(node) && node != rootNode) {
                     ArrayList<Integer> connectedNodes = map.get(node);
-                    findPath(connectedNodes, dest, stepIndex, steps++);
+                    if (findPath(connectedNodes, dest, stepIndex, ++steps)) break;
                 }
             }
         }
+        return false;
     }
 }
